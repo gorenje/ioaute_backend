8b6093300935c7f329d3875cd3261482632785ec
Gerrit Riessen <gerrit.riessen@gmail.com>
2010-12-15 12:02:15 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pubme::Application.config.secret_token = '18dd6e9aea00918c030d6524a05202f08b5bd033dac4a18ccb95ab5479690a3428838c61ec9d3c36600ce30f7d857c381d944b49f9f61cb51a978fc79a262471'
