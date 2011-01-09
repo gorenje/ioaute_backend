142fcac3a6decc8bf96e8e885e761443e6fe847a
Gerrit Riessen <gerrit.riessen@gmail.com>
2011-01-09 15:09:55 +0100

    fixed confirmation url?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,7 @@ Pubme::Application.configure do
   # Specifies the header that your server uses for sending files
   config.action_dispatch.x_sendfile_header = "X-Sendfile"
 
-  config.action_mailer.default_url_options = { :host => 'http://pubme.2monki.es' }
+  config.action_mailer.default_url_options = { :host => 'pubme.2monki.es' }
   config.action_mailer.delivery_method = :sendmail
 
   # For nginx:
