ee5280d8f627aa28c02eec6d2fb6b6c4446ee72b
Gerrit Riessen <gerrit.riessen@gmail.com>
2011-01-11 17:53:54 +0100

    cleaned up the css for printing the html page of a publication

--- app/helpers/publications_helper.rb
+++ app/helpers/publications_helper.rb
@@ -1,2 +1,9 @@
 module PublicationsHelper
+  # these classes are used by the print css to force page breaks after each page 
+  # except the last page.
+  def class_for_page_number(idx, total_pages)
+    return "first_page" if idx == 0
+    return "last_page" if idx == total_pages-1
+    "not_first_or_last_page"
+  end
 end
