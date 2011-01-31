b6d58a5a0ae572fa66acb3d2762b07aedd48f9c9
Gerrit Riessen <gerrit.riessen@gmail.com>
2011-01-31 18:30:03 +0100

    tweet link conversion and some more font sizes for the heeader

--- lib/string_extensions.rb
+++ lib/string_extensions.rb
@@ -2,5 +2,10 @@ module StringExtensions
   def remove_indent
     self =~ /\A([ \t]+)/ ? gsub(/\n#{$1}/, "\n").strip : self
   end
+  
+  def convert_urls_to_links(html_option_string = "")
+    self.gsub(/((https?|ftps?):\/\/[^[:space:]]+)/, 
+              "<a #{html_option_string} href='\\1'>\\1</a>")
+  end
 end
 String.send(:include, StringExtensions)
