c69c939467838ad73a3dacc97af6e6ccf9d42851
Gerrit Riessen <gerrit.riessen@gmail.com>
2011-02-04 13:19:26 +0100

    fix long lines and ensure that they get wrapped around

--- lib/string_extensions.rb
+++ lib/string_extensions.rb
@@ -3,6 +3,10 @@ module StringExtensions
     self =~ /\A([ \t]+)/ ? gsub(/\n#{$1}/, "\n").strip : self
   end
   
+  def nbsp_leading_whitespace
+    self.gsub(/(\n[ \t]+)/) { |whitespace| whitespace.gsub(/[ \t]/, '&nbsp;') }
+  end
+  
   def convert_urls_to_links(html_option_string = "")
     self.gsub(/((https?|ftps?):\/\/[^[:space:]]+)/, 
               "<a #{html_option_string} href='\\1'>\\1</a>")
