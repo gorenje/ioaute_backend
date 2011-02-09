a38ee3d6a85cdcb95494af1ee43d454c36bb33ec
Gerrit Riessen <gerrit.riessen@gmail.com>
2011-02-09 17:46:09 +0100

    support you tube properties

--- vendor/plugins/page_elements/app/models/you_tube_video_element.rb
+++ vendor/plugins/page_elements/app/models/you_tube_video_element.rb
@@ -1,4 +1,5 @@
 class YouTubeVideoElement < PageElement
+  
   class << self
     def extract_data_from_params(params)
       { :thumbnail => { 
@@ -8,8 +9,13 @@ class YouTubeVideoElement < PageElement
         :content => { 
           "5" => params["m_video"]
         },
-        :title => params["m_title"],
-        :uploader => params["m_owner"],
+        :artist => { 
+          :url => params["m_artist_url"],
+          :name => params["m_artist_name"],
+        },
+        :title            => params["m_title"],
+        :uploader         => params["m_owner"],
+        :m_search_engines => params["m_search_engines"],
       }
     end
   end
@@ -30,16 +36,41 @@ class YouTubeVideoElement < PageElement
     extra_data["uploader"]
   end
 
-  def show_search_links?
-    true
+  def show_links?
+    edata = extra_data
+    ((edata["m_search_engines"].to_i || 0) > 0) || (edata["artist"] && 
+                                                    !edata["artist"]["url"].blank?)
   end
   
   def links
+    link_dumper = []
+    edata = extra_data
+    srch_flag = edata["m_search_engines"].to_i
+    
+    # this goes all the back to the Xib for the youtube video property window .. it
+    # defines tags and these tags are bitwise unique. These tags are dragged all the
+    # way from the window to here and they need to match.
+    # Amazon    => 1
+    # Google    => 2
+    # Bing      => 4
+    # SevenLoad => 8
+    # DuckDuckGo => 16
     srch_title = CGI.escape(title)
     [ 
      ["@Amazon", "http://www.amazon.de/s?field-keywords=#{srch_title}"],
-     ["@Google", "http://google.com/search?q=#{srch_title}"] 
-    ]
+     ["@Google", "http://google.com/search?q=#{srch_title}"],
+     ["@Bing", ""],
+     ["@SevenLoad", ""],
+     ["@DuckDuckGo", ""]
+    ].each_with_index do |link_info, idx|
+      link_dumper << link_info if (srch_flag & (2 ** idx)) > 0
+    end
+
+    if edata["artist"] && !edata["artist"]["url"].blank?
+      artist_name = edata["artist"]["name"].blank? ? "@Artist" :edata["artist"]["name"]
+      link_dumper << [artist_name, edata["artist"]["url"]]
+    end
+    link_dumper
   end
   
   def self._type
