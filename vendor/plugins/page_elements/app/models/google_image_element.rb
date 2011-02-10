9d433679c6b224692d30ea4be6210cac79e197a3
Gerrit Riessen <gerrit.riessen@gmail.com>
2011-02-10 17:01:57 +0100

    support for google image properties:

--- vendor/plugins/page_elements/app/models/google_image_element.rb
+++ vendor/plugins/page_elements/app/models/google_image_element.rb
@@ -3,13 +3,15 @@ class GoogleImageElement < PageElement
   class << self
     def extract_data_from_params(params)
       { :image_url => params["m_imageUrl"],
+        :dest_url => params["m_destUrl"] || params["m_urlString"],
       }
     end
   end
 
   def _json
     edata = extra_data
-    { "unescapedUrl" => edata["image_url"], :id => id_str }
+    { "dest_url" => edata["image_url"] }.merge(edata).
+      merge({ "unescapedUrl" => edata["image_url"], :id => id_str })
   end
 
   def self._type
