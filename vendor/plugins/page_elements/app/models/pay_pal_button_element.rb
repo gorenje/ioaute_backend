954ab06041aab76c452ea002d1fac1798c331bb9
Gerrit Riessen <gerrit.riessen@gmail.com>
2011-02-14 17:44:45 +0100

    working version of the paypal button

--- vendor/plugins/page_elements/app/models/pay_pal_button_element.rb
+++ vendor/plugins/page_elements/app/models/pay_pal_button_element.rb
@@ -2,9 +2,10 @@ class PayPalButtonElement < PageElement
   class << self
     def extract_data_from_params(params)
       { 
-        :email    => params["m_email"],
-        :currency => params["m_currency"],
-        :locale   => params["m_locale"],
+        :email      => params["m_email"],
+        :currency   => params["m_currency"],
+        :locale     => params["m_locale"],
+        :image_size => params["m_image_size"],
       }
     end
   end
@@ -23,5 +24,15 @@ class PayPalButtonElement < PageElement
   
   def currency
     extra_data["currency"]
+  end
+  
+  def image_url
+    image_size = extra_data["image_size"]
+    case image_size
+    when "large"         then "https://www.paypal.com/en_US/i/btn/btn_donate_LG.gif"
+    when "large_with_cc" then "https://www.paypal.com/en_US/i/btn/btn_donateCC_LG.gif"
+    else
+      "https://www.paypal.com/en_US/i/btn/btn_donate_SM.gif"
+    end
   end
 end
