dc66ecb8976afee3a750e58ba6d93a85505b58e2
Gerrit Riessen <gerrit.riessen@gmail.com>
2011-02-18 17:54:23 +0100

    highlght element is clickable

--- vendor/plugins/page_elements/app/models/highlight_element.rb
+++ vendor/plugins/page_elements/app/models/highlight_element.rb
@@ -3,7 +3,13 @@ class HighlightElement < PageElement
 
   class << self
     def extract_data_from_params(params)
-      obtain_colors_from_params(params)
+      obtain_colors_from_params(params).
+        merge({ 
+                :link_url       => params["m_link_url"],
+                :show_as_border => params["m_show_as_border"],
+                :border_width   => params["m_border_width"],
+                :clickable      => params["m_is_clickable"],
+              })
     end
   end
 
@@ -13,5 +19,17 @@ class HighlightElement < PageElement
 
   def self._type
     "HighlightTE"
+  end
+  
+  def is_clickable?
+    extra_data["clickable"].to_i > 0
+  end
+  
+  def link_url
+    extra_data["link_url"]
+  end
+  
+  def border_width_pixel
+    extra_data["border_width"]
   end
 end
